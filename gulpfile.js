85f28a770fbed7069eea9cd28fb6f72727d44cab
srbberlin <info@hans-schrieber.de>
2018-02-15 13:12:36 +0100

    Adapted to the projects structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,76 +1,29 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
-const sourcemaps = require('gulp-sourcemaps')
-const babel = require('gulp-babel')
-//const concat = require('gulp-concat')
 const browserSync = require('browser-sync')
-//bootconst sequence = require('gulp-series')
-const del = require('del')
 
 var config = {
   base:   __dirname + '/',
-  html:   '**/*.html',
-  cssin:  'sass/**/*.sass',
-  cssout: 'css/',
-  jsin:   'babel/**/*.js',
-  jsout:  'js/',
+  html:   __dirname + '/**/*.html',
+  css:    __dirname + '/css/**/*.css',
+  js:     __dirname + '/js/**/*.js',
+  img:    __dirname + '/img/**/*'
 }
 
 gulp.task('reload', function() {
-  console.log('')
+  console.log('reload')
   browserSync.reload()
 })
 
-gulp.task('serve', ['sass', 'babel'], function() {
+gulp.task('serve', function() {
   console.log('serve')
   browserSync({
     server: config.base
   })
 
   gulp.watch(config.html, ['reload'])
-  gulp.watch(config.jsin, ['babel', 'reload'])
-  gulp.watch(config.cssin, ['sass', 'reload'])
+  gulp.watch(config.css, ['reload'])
+  gulp.watch(config.js, ['reload'])
+  gulp.watch(config.img, ['reload'])
 })
-
-gulp.task('sass', function() {
-  let path = config.base + config.cssin
-  console.log('sass', path)
-  return gulp.src(path)
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    //.pipe(autoprefixer({
-    //  browsers: ['last 3 versions']
-    //}))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.base + config.cssout))
-    .pipe(browserSync.stream())
-})
-
-gulp.task('babel', function() {
-  let path = config.base + config.jsin
-  console.log('babel', path)
-  return gulp.src(path)
-    .pipe(babel())
-    .pipe(gulp.dest(config.base + config.jsout))
-    .pipe(browserSync.stream())
-})
-
-gulp.task('clean', function() {
-  let paths = [config.base + config.jsout + '/*.js', config.base + config.cssout + '/*.css']
-  let res;
-  console.log('clean', [config.base + config.js + '/*.js', config.base + config.css + '/*.css'])
-  res = del(paths)
-  res.then(function (a) {
-    console.log('clean', a)
-  }).catch( function (a) {
-    console.log('clean', a)
-  })
-  return res
-})
-
-gulp.task('build', ['babel', 'sass'])
 
 gulp.task('default', ['serve'])
-
-  
-  
